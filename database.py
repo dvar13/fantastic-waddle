02174c5f979422c3a9ad15d0b209f6b3f97d5439
Ubuntu <ubuntu@ip-172-31-47-58.ec2.internal>
2026-02-26 17:37:40 +0000

    Correcion DB Conector

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
@@ -10,12 +10,10 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:3306/DB_NAME}"
-)
+# Fixed the variable reference and the trailing brace
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
 
 engine = create_engine(
     DATABASE_URL,
-    echo= True
+    echo=True
 )
